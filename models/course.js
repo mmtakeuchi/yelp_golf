2d54e422273c636f113ad319bb4e70997758c7c9
mmtakeuchi <mikemtaki@gmail.com>
2021-02-03 14:00:19 -0700

    deleting a golf course, will now delete reviews for the course

--- models/course.js
+++ models/course.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const Review = require("./review");
 const Schema = mongoose.Schema;
 
 const CourseSchema = new Schema({
@@ -13,6 +14,16 @@ const CourseSchema = new Schema({
       ref: "Review",
     },
   ],
+});
+
+CourseSchema.post("findOneAndDelete", async function (doc) {
+  if (doc) {
+    await Review.deleteMany({
+      _id: {
+        $in: doc.reviews,
+      },
+    });
+  }
 });
 
 module.exports = mongoose.model("Course", CourseSchema);
