307d7558af2eecc690bd608d40ffb4074a9c78c2
mmtakeuchi <mikemtaki@gmail.com>
2021-02-04 12:41:40 -0700

    create register form with validations

--- app.js
+++ app.js
@@ -6,9 +6,13 @@ const session = require("express-session");
 const flash = require("connect-flash");
 const ExpressError = require("./utils/ExpressError");
 const methodOverride = require("method-override");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user");
 
-const courses = require("./routes/courses");
-const reviews = require("./routes/reviews");
+const userRoutes = require("./routes/users");
+const courseRoutes = require("./routes/courses");
+const reviewRoutes = require("./routes/reviews");
 
 mongoose.connect("mongodb://localhost:27017/yelp-golf", {
   useNewUrlParser: true,
@@ -46,14 +50,22 @@ const sessionConfig = {
 app.use(session(sessionConfig));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req, res, next) => {
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
   next();
 });
 
-app.use("/courses", courses);
-app.use("/courses/:id/reviews", reviews);
+app.use("/", userRoutes);
+app.use("/courses", courseRoutes);
+app.use("/courses/:id/reviews", reviewRoutes);
 
 app.get("/", (req, res) => {
   res.render("home");
---
+++ routes/users.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router();
+const catchAsync = require("../utils/catchAsync");
+const User = require("../models/user");
+
+router.get("/register", (req, res) => {
+  res.render("users/register");
+});
+
+router.post(
+  "/register",
+  catchAsync(async (req, res) => {
+    try {
+      const { email, username, password } = req.body;
+      const user = new User({ email, username });
+      const registeredUser = await User.register(user, password);
+      req.flash("success", "Welcome to YelpGolf!");
+      res.redirect("/courses");
+    } catch (e) {
+      req.flash("error", e.message);
+      res.redirect("register");
+    }
+  })
+);
+
+module.exports = router;
