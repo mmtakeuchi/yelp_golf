a9c33731a1bfa5a12af9aed9ecbdd669c7250c37
mmtakeuchi <mikemtaki@gmail.com>
2021-02-09 15:35:11 -0700

    connect course location to cluster map

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -10,11 +10,11 @@ map.on("load", function () {
   // Add a new source from our GeoJSON data and
   // set the 'cluster' option to true. GL-JS will
   // add the point_count property to your source data.
-  map.addSource("campgrounds", {
+  map.addSource("courses", {
     type: "geojson",
     // Point to GeoJSON data. This example visualizes all M1.0+ earthquakes
     // from 12/22/15 to 1/21/16 as logged by USGS' Earthquake hazards program.
-    data: campgrounds,
+    data: courses,
     cluster: true,
     clusterMaxZoom: 14, // Max zoom to cluster points on
     clusterRadius: 50, // Radius of each cluster when clustering points (defaults to 50)
@@ -23,7 +23,7 @@ map.on("load", function () {
   map.addLayer({
     id: "clusters",
     type: "circle",
-    source: "campgrounds",
+    source: "courses",
     filter: ["has", "point_count"],
     paint: {
       // Use step expressions (https://docs.mapbox.com/mapbox-gl-js/style-spec/#expressions-step)
@@ -47,7 +47,7 @@ map.on("load", function () {
   map.addLayer({
     id: "cluster-count",
     type: "symbol",
-    source: "campgrounds",
+    source: "courses",
     filter: ["has", "point_count"],
     layout: {
       "text-field": "{point_count_abbreviated}",
@@ -59,7 +59,7 @@ map.on("load", function () {
   map.addLayer({
     id: "unclustered-point",
     type: "circle",
-    source: "campgrounds",
+    source: "courses",
     filter: ["!", ["has", "point_count"]],
     paint: {
       "circle-color": "#11b4da",
@@ -76,7 +76,7 @@ map.on("load", function () {
     });
     const clusterId = features[0].properties.cluster_id;
     map
-      .getSource("campgrounds")
+      .getSource("courses")
       .getClusterExpansionZoom(clusterId, function (err, zoom) {
         if (err) return;
 
