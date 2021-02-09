b6fc4b3662f15e6984e7ce3abe5354ea41f4a976
mmtakeuchi <mikemtaki@gmail.com>
2021-02-08 19:58:48 -0700

    create new image schema in course schema to create thumbail sizing

--- models/course.js
+++ models/course.js
@@ -2,9 +2,18 @@ const mongoose = require("mongoose");
 const Review = require("./review");
 const Schema = mongoose.Schema;
 
+const ImageSchema = new Schema({
+  url: String,
+  filename: String,
+});
+
+ImageSchema.virtual("thumbnail").get(function () {
+  return this.url.replace("/upload", "/upload/w_200");
+});
+
 const CourseSchema = new Schema({
   name: String,
-  images: [{ url: String, filename: String }],
+  images: [ImageSchema],
   description: String,
   location: String,
   price: Number,
